2ad1605c59dd07eb93b89e157263374a0a6ab7f2
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2023-02-12 12:43:02 +0000

    Deploying to gh-pages from @ yuriiavr/phonebook@6b180bacd27acc22a5747d914fdf4eaaa7406574 🚀

---
+++ static/js/672.46a6fbb2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[672],{1672:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var a="NotFoundPage_nf__title__FRJs4",r=n(184);function u(){return(0,r.jsx)("h1",{className:a,children:"404 Not found page :("})}}}]);
+//# sourceMappingURL=672.46a6fbb2.chunk.js.map
